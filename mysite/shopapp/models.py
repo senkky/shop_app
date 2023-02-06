e9b82408db2176edfc70de94fb94d61d24c3aeca
Aleksandr Trofimov <senkevich2004@yandex.ru>
2023-02-06 14:03:08 +0300

    Task 4

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -8,11 +8,20 @@ class Product(models.Model):
         # verbose_name_plural = "products"
 
     name = models.CharField(max_length=100)
-    discription = models.TextField(null=False, blank=True)
+    description = models.TextField(null=False, blank=True)
     price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
     discount = models.SmallIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     archived = models.BooleanField(default=False)
+
+    # @property
+    # def description_short(self) -> str:
+    #     if len(self.description) < 48:
+    #         return self.description
+    #     return self.description[:48] + "..."
+
+    def __str__(self) -> str:
+        return f"Product(pk={self.pk}, name={self.name!r})"
 
 class Order(models.Model):
     delivery_address = models.TextField(null=True, blank=True)
