6dda9c3299c700f8fca9395fe1c00fec7b15fc5d
senkky <110473903+senkky@users.noreply.github.com>
2023-07-02 13:34:06 +0300

    Delete 17_DataExport/djsale directory

--- 17_DataExport/djsale/app_rss/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppRssConfig(AppConfig):
-    name = 'app_rss'
--- 17_DataExport/djsale/app_sitemap/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppSitemapConfig(AppConfig):
-    name = 'app_sitemap'
